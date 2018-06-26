9f3124a907022dbfceae6b54f1dc6b3f491d0781
zhy <>
2018-06-26 20:58:50 +0800

    新增游客模式

---
+++ js/config.js
@@ -0,0 +1,16 @@
+(function(){
+    window.gg=window.gg || {
+
+        };
+    window.gg.config={
+        width:640,
+        height:1040,
+        showType:0, // 0-锟斤拷锟斤拷锟斤拷 锟竭筹拷锟斤拷
+        clientWidth:document.documentElement.clientWidth,
+        clientHeight:document.documentElement.clientHeight,
+        appid:'wxb069c56e06385915',
+        projectUrl:'https://www.ludianvr.com',
+        shareGradeMin : 45,
+        shareGradeMax : 60
+    };
+})();
